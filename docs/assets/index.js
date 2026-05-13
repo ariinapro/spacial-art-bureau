7ae9e5874b51e94491fa08ededfd43806aa096ea
ariinapro <arinaprokopenko00@gmail.com>
2026-05-13 04:32:22 +0300

    Delete myFunction from index.js
    Remove myFunction that toggles navbar responsiveness.

--- docs/assets/index.js
+++ docs/assets/index.js
@@ -1,8 +1,1 @@
-function myFunction() {
-  var x = document.getElementById("navbar-instance");
-  if (x.className === "navbar-container") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar-container";
-  }
-}
+
